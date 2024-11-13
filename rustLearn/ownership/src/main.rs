6519a82c0083f6f710c1091aeb0b5bd16f6fc6a8
matthewhua <57536009+matthewhua@users.noreply.github.com>
2024-11-13 17:53:25 +0800

    rust 提交啊啊啊啊

---
+++ rustLearn/ownership/src/main.rs
@@ -0,0 +1,19 @@
+mod ownership1;
+
+use std::iter::Iterator;
+
+fn main() {
+    let data = vec![1,2,3,4];
+    let data1 = data;
+    println!("sum of data1: {}", sum(data1));
+
+    // 下面两句无法编译通过
+    // println!("data1: {:?}", data1);
+    // 不能使用已经使用过的变量
+    // println!("sum of data: {}", sum(data));
+}
+
+
+fn sum(data: Vec<u32>) -> u32 {
+    data.iter().fold(0, |acc, &x| acc + x)
+}
---
+++ rustLearn/ownership/src/ownership1.rs
@@ -0,0 +1,20 @@
+fn main() {
+    let data = vec![10, 42, 9, 8];
+    let v = 42;
+
+    if let Some(pos) = find_pos(data, v) {
+        println!("Found {} at {}", v, pos);
+    } else {
+        println!("{} not found", v);
+    }
+}
+
+fn find_pos(data: Vec<i32>, v: i32) -> Option<usize> {
+    for (pos, item) in data.iter().enumerate() {
+        if *item == v {
+            return Some(pos)
+        }
+    }
+
+    None
+}
